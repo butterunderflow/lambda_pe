af461dcc1f580575e7242e81dd92d628b72e4768
butterunderflow <azhong.934@gmail.com>
2024-02-17 16:43:59 +0800

    use closure to represent function value for easier debuging

--- lib/expr1.ml
+++ lib/expr1.ml
@@ -1,4 +1,5 @@
 open Sexplib.Conv
+module StrSet = Set.Make (String)
 
 type ty =
   | TInt
@@ -13,10 +14,30 @@ type expr =
   | EApp of expr * expr
 [@@deriving sexp]
 
+let get_free_vars (e : expr) : string list =
+  let vars = ref StrSet.empty in
+  let rec go e env =
+    match e with
+    | EInt _ -> ()
+    | EVar x -> if not (StrSet.mem x env) then vars := StrSet.add x !vars
+    | ELam (x, _, e1) -> go e1 (StrSet.add x env)
+    | ELet (x, e0, e1) ->
+        go e0 env;
+        go e1 (StrSet.add x env)
+    | EApp (e0, e1) ->
+        go e0 env;
+        go e1 env
+  in
+  go e StrSet.empty;
+  StrSet.fold (fun x acc -> x :: acc) !vars []
+
 type value =
   | VInt of int
-  | VFun of (value -> value)
-[@@deriving sexp]
+  | VFun of closure
+
+and env = (string * value) list
+
+and closure = env * string * expr [@@deriving sexp]
 
 let get_int (v : value) =
   match v with
@@ -28,8 +49,6 @@ let get_fun (v : value) =
   | VInt _ -> failwith "neverreach"
   | VFun f -> f
 
-type env = (string * value) list
-
 let empty_env = []
 
 let get x env = List.assoc x env
@@ -40,14 +59,18 @@ let rec eval (e : expr) env : value =
   match e with
   | EInt v -> VInt v
   | EVar x -> get x env
-  | ELam (x, _, e) -> VFun (fun x_val -> eval e (push x x_val env))
+  | ELam (x, _, e0) ->
+      let vars = get_free_vars e in
+      let captures = List.map (fun x -> (x, get x env)) vars in
+      VFun (captures, x, e0)
   | ELet (x, e0, e1) ->
       let x_value = eval e0 env in
       eval e1 (push x x_value env)
   | EApp (e0, e1) ->
       let f = eval e0 env in
       let arg = eval e1 env in
-      f |> get_fun arg
+      let captures, x, body = get_fun f in
+      eval body (push x arg captures)
 
 (* inline tests *)
 
@@ -57,4 +80,4 @@ let%expect_test "Test: eval lambda" =
   eval (EInt 0) empty_env |> print_value;
   [%expect {| (VInt 0) |}];
   eval (ELam ("x", TInt, EVar "x")) empty_env |> print_value;
-  [%expect {| (VFun <fun>) |}]
+  [%expect {| (VFun (() x (EVar x))) |}]
