666932cb01321610320f2b4615c12f7073046c67
butterunderflow <azhong.934@gmail.com>
2024-03-07 10:07:47 +0800

    export function to js by jsoo

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,32 @@
-let () = print_endline "Hello, World!"
+open Js_of_ocaml
+open Lambda_pe_lib
+
+let handle program =
+  Printf.printf "%s\n" program;
+  flush stdout
+
+let eval_expr1 program =
+  let e = Parsing.parse_expr1_string program in
+  Expr1.(eval e empty_env |> sexp_of_value |> Common.string_of_sexp)
+
+let eval_expr2 program =
+  let e = Parsing.parse_expr2_string program in
+  Expr2.(eval e empty_env |> sexp_of_value |> Common.string_of_sexp)
+
+let bta program =
+  let e = Parsing.parse_expr1_string program in
+  Bta.InferBTA.analysis e |> Expr2.sexp_of_expr |> Common.string_of_sexp
+
+let main () =
+  Js.export "programHandler"
+    (object%js
+       method naiveHandle = handle
+
+       method evalExpr1 = eval_expr1
+
+       method evalExpr2 = eval_expr2
+
+       method bta = bta
+    end)
+
+let () = main ()
--- lib/common.ml
+++ lib/common.ml
@@ -1,2 +1,4 @@
+let string_of_sexp = Sexplib.Sexp.to_string_hum ?indent:(Some 2)
+
 let print_sexp s =
   Sexplib.Sexp.to_string_hum ?indent:(Some 2) s |> print_string
