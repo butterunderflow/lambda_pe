23a61743a675be77324e599f0288f7c16d4c10ae
butterunderflow <azhong.934@gmail.com>
2024-09-07 22:16:28 +0800

    make test more readable

--- test/bta_test.ml
+++ test/bta_test.ml
@@ -10,40 +10,33 @@ let%expect_test "Test: inference binding time annotation" =
     Ann.sexp_of_t a |> print_sexp;
     print_newline ()
   in
-  infer (EConst (CInt 0)) empty_env |> print_result;
+  let print_infered str =
+    infer (Parsing.parse_expr1_string str) empty_env |> print_result
+  in
+  print_infered {|0|};
   [%expect {|
       (SConst (CInt 0))
       S |}];
-  infer (ELet ("x", EConst (CInt 0), EVar "x")) empty_env |> print_result;
+  print_infered {| let x = 0 in x |};
   [%expect {|
       (SLet x (SConst (CInt 0)) (Var x))
       S |}];
-  infer
-    (EAnn (ELam ("x", ELam ("y", EConst (CInt 0))), Func (S, D)))
-    empty_env
-  |> print_result;
+  print_infered {| ((fun x -> fun y -> 0) : S -> D) |};
   [%expect
     {|
       (SLam x (DLam y (DLift (SConst (CInt 0)))))
       (Func (S D)) |}];
-  infer
-    (EAnn (ELam ("x", ELam ("y", EConst (CInt 0))), Func (D, Func (S, S))))
-    empty_env
-  |> print_result;
+  print_infered {| ((fun x -> fun y -> 0): D -> S -> S) |};
   [%expect
     {|
       (SLam x (SLam y (SConst (CInt 0))))
       (Func (D (Func (S S)))) |}];
-  infer (EOp (OAdd, [ EConst (CInt 7); EConst (CInt 3) ])) empty_env
-  |> print_result;
+  print_infered {| 7 + 3 |};
   [%expect
     {|
       (SOp OAdd ((SConst (CInt 7)) (SConst (CInt 3))))
       S |}];
-  infer
-    (EOp (OAdd, [ EAnn (EConst (CInt 7), D); EConst (CInt 3) ]))
-    empty_env
-  |> print_result;
+  print_infered {| (7 : D) + 3 |};
   [%expect
     {|
       (DOp OAdd ((DLift (SConst (CInt 7))) (DLift (SConst (CInt 3)))))
